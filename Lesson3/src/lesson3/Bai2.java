ffafe1d869bd7ff7ffb9c8fd86c264ba73a88bd6
Codese <thienhadenhatvonhi@gmail.com>
2019-11-18 20:53:33 +0700

    'lesson2'

---
+++ Lesson3/src/lesson3/Bai2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson3;
+
+/**
+ *
+ * @author Wind
+ */
+public class Bai2 {
+    public static void main(String[] args) {
+        
+        // tạo TK git
+        // Tải Git 
+        // tạo repository
+        // clone repo về máy
+        // vứt hết project vào thư mục đấy
+        // đồng bộ hóa máy tính với TK GIT:
+          //git add .
+          //git commit -m 'TenPhienBan'
+          //git push origin master
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
